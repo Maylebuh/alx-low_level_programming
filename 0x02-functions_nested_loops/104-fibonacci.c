814006f1a6b5d26749dae525d91404cd59cff03e
Maylebuh <malebomawela96@gmail.com>
2024-03-15 10:00:13 -0700

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - The main function
+ * main _ The main function
  * 1 and 2, seperated by a comma
  * Return: 0.
  */
 
 int main(void)
-
 {
 	int count;
 	unsigned long fib1 = 0, fib2 = 1, sum;
 	unsigned long fib1_half1, fib2_half1, fib2_half2;
 	unsigned long half1, half2;
+
 
 	for (count = 0; count < 92; count++)
 	{
